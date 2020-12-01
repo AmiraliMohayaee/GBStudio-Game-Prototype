00b957167f3d8f6c978b14d76f752bec3b895632
Amirali Mohayaee <amig90@yahoo.com>
2020-12-01 13:05:16 +0000

    More assets and changes

---
+++ assets/backgrounds/GBtilesheet4colours.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="GBtilesheet4colours(1)" tilewidth="8" tileheight="8" tilecount="70400" columns="220">
+ <image source="../../Tile Sets/GBtilesheet4colours(1).png" width="1760" height="2560"/>
+</tileset>
