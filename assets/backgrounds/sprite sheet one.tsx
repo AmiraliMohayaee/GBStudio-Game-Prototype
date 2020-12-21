c5abcd7a09d7390a3da45526e27c8f92558f3297
Amir <amig90@yahoo.com>
2020-12-21 03:35:33 +0000

    Trying out new asset creation

---
+++ assets/backgrounds/sprite sheet one.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="sprite sheet one" tilewidth="8" tileheight="8" tilecount="768" columns="32">
+ <image source="../../Tile Sets/sprite sheet one.png" width="256" height="192"/>
+</tileset>
